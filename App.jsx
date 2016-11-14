f4c2e0e74de84110be0ededac36d0f2e86f8092f
Gabriel CHAN <ycchangabriel@gmail.com>
2016-11-14 12:59:24 +0800

    added Avatar class from npm control I count this as finish avatar feature

--- App.jsx
+++ App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'whatwg-fetch';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import Avatar from 'react-avatar';
 
 
-class Avatar extends React.Component{
-  render() {
-    return (
-      <img src={this.props.url} />
-    );
-  }
-}
+// class Avatar extends React.Component{
+//   render() {
+//     return (
+//       <img src={this.props.url} />
+//     );
+//   }
+// }
 
 class App extends React.Component {
 	constructor() {
@@ -24,14 +25,17 @@ class App extends React.Component {
 	}
 
   avatarFormatter(cell, row){
-    return <Avatar url={cell}/>;
+    return (
+      <a href={row.html_url}>
+        <Avatar src={cell} round={true} name={row.login}/>
+      </a>
+    );
   }
 
 	render() {
 		return (
       <BootstrapTable data={ this.state.rows }>
-          <TableHeaderColumn dataField='avatar_url' dataFormat={this.avatarFormatter}>Avatar</TableHeaderColumn>
-          <TableHeaderColumn dataField='id' isKey={ true }>User ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='avatar_url' dataFormat={this.avatarFormatter} isKey={true}>Avatar</TableHeaderColumn>
           <TableHeaderColumn dataField='login'>Login Name</TableHeaderColumn>
           <TableHeaderColumn dataField='type'>User Type</TableHeaderColumn>
       </BootstrapTable>
